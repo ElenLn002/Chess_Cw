0a919466e34646d4a0b8aa606e8995ca1597b290
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-05 02:08:09 +0400

    Definitions.hpp

--- GUI/Definitions.hpp
+++ GUI/Definitions.hpp
@@ -6,4 +6,12 @@ const int BOARD_SIZE = 8;
 const int SQUARE_SIZE = 50;
 const int FRAME_SIZE = 10;
 
+enum class PieceColour { White, Black };
+enum class PieceType { Pawn, Rook, Knight, Bishop, Queen, King };
+
+struct Position {
+    int row;
+    int col;
+};
+
 #endif // !DEFINITIONS_HPP
